ea447aa11bf7abdd64eacb6df480bb031a59a0d8
yoonl22 <tylee0840@gmail.com>
2024-06-11 21:47:04 +0900

    add fastapi server

--- 003 Code/converter/app/api/translate/route.ts
+++ 003 Code/converter/app/api/translate/route.ts
@@ -1,14 +1,16 @@
-<<<<<<< HEAD
 import { NextResponse } from 'next/server';
 import { TranslateClient, TranslateTextCommand } from '@aws-sdk/client-translate';
 import path from "path";
 import dotenv from "dotenv";
-dotenv.config({ path: path.join('/Users/berey/capstone/come-capstone24-converter/003 Code/converter', '.env') });
+// dotenv.config({ path: path.join('/Users/berey/capstone/come-capstone24-converter/003 Code/converter', '.env') });
+// dotenv.config();
+dotenv.config({ path: '/home/tylee/caps/.env' });
+
 const translate = new TranslateClient({
-    region: process.env.REGION,
+    region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 });
 
@@ -29,35 +31,3 @@ export async function POST(req) {
         return new Response(JSON.stringify({ error: 'Error translating text' }), { status: 500 });
     }
 }
-=======
-    import { NextResponse } from 'next/server';
-    import { TranslateClient, TranslateTextCommand } from '@aws-sdk/client-translate';
-    import path from "path";
-    import dotenv from "dotenv";
-    dotenv.config({ path: path.join('/Users/iyeongho/lvnvn/come-capstone24-converter/003 Code/converter/.env', '.env') });
-    const translate = new TranslateClient({
-        region: process.env.REGION,
-        credentials: {
-            accessKeyId: process.env.ACCESS_KEY_ID,
-            secretAccessKey: process.env.SECRET_ACCESS_KEY
-        }
-    });
-
-    export async function POST(req) {
-        try {
-            const { text } = await req.json();
-            console.log('Text to translate:', text);  // 디버깅을 위한 로그 추가
-            const command = new TranslateTextCommand({
-                Text: text,
-                SourceLanguageCode: 'auto',
-                TargetLanguageCode: 'ko',  // 한국어로 번역
-            });
-            const result = await translate.send(command);
-            console.log('Translation result:', result);  // 디버깅을 위한 로그 추가
-            return NextResponse.json({ translatedText: result.TranslatedText });
-        } catch (error) {
-            console.error('Error translating text:', error);
-            return new Response(JSON.stringify({ error: 'Error translating text' }), { status: 500 });
-        }
-    }
->>>>>>> 9d0c8bbc333ee0634a825299bfc216a738370d30
--- 003 Code/converter/app/api/upload_s3/route.ts
+++ 003 Code/converter/app/api/upload_s3/route.ts
@@ -3,19 +3,22 @@ import path from "path";
 import AWS from "aws-sdk";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.join('/Users/berey/capstone/come-capstone24-converter/003 Code/onverter', '.env') });
+// dotenv.config({ path: path.join('/Users/berey/capstone/come-capstone24-converter/003 Code/onverter', '.env') });
+dotenv.config({ path: '/home/tylee/caps/.env' });
+// dotenv.config();
 
 // AWS S3 설정
 const s3 = new AWS.S3({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  region: process.env.REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 });
 
 
 export const POST = async (req, res) => {
   const formData = await req.formData();
   console.log('test_s3!!!');
+  console.log(process.env.AWS_ACCESS_KEY_ID);
   const file = formData.get("file");
   if (!file) {
     return NextResponse.json({ error: "No files received." }, { status: 400 });
@@ -27,12 +30,12 @@ export const POST = async (req, res) => {
   
   try {
     const s3Params = {
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: process.env.S3_BUCKET,
       Key: `uploads/11-04-2024/${filename}`,
       Body: buffer
     };
     await s3.upload(s3Params).promise();
-    const fileurl = `https://${process.env.S3_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/${s3Params.Key}`;
+    const fileurl = `https://${process.env.S3_BUCKET}.s3.ap-northeast-2.amazonaws.com/${s3Params.Key}`;
    
     // console.log("test + 1: " +  fileurl);
     return NextResponse.json({fileurl, Message: "Success", status: 201 });
