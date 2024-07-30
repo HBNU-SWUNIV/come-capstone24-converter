010a8ddb6ba59886e495a75f5f63dce612894d8b
lvnvn177 <vheh502@gmail.com>
2024-07-30 09:31:26 +0900

    llama_func

--- 003 Code/ai-serve/main.py
+++ 003 Code/ai-serve/main.py
@@ -1,13 +1,18 @@
 from collections import OrderedDict
 import json
-
+import logging
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from routers.s3 import s3r
+from routers.llama import llamaR
 from services.summarize import summarize
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -27,7 +32,9 @@ app.add_middleware(
 )
 
 app.include_router(s3r)
-
+logging.info('s3 router included')
+app.include_router(llamaR)
+logging.info('llama router included')
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
--- 003 Code/converter/app/upload/page.tsx
+++ 003 Code/converter/app/upload/page.tsx
@@ -4,37 +4,36 @@ import { useSearchParams } from "next/navigation";
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-// import axios from 'axios';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function UploadPage() {
-    const [numPages, setNumPages] = useState(null);
-    const [pageScale, setPageScale] = useState(2.0); // 기본 스케일 값을 2.0로 설정하여 기본 크기를 증가시킵니다.
-    const [selectedText, setSelectedText] = useState('');
-    const [translatedText, setTranslatedText] = useState('');
-    const [summarizedText, setSummarizedText] = useState('');
-    const [question, setQuestion] = useState('');
-    const [answer, setAnswer] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [numPages, setNumPages] = useState(null); // @State, 현재 페이지 수  
+    const [pageScale, setPageScale] = useState(2.0); // @State, 현재 Scale, 초기값 2.0
+    const [selectedText, setSelectedText] = useState(''); // @State, 현재 드래그하여 선택한 텍스트
+    const [translatedText, setTranslatedText] = useState(''); // @State, 현재 번역된 텍스트
+    const [summarizedText, setSummarizedText] = useState(''); // @State, 현재 요약된 텍스트
+    const [question, setQuestion] = useState(''); // @State, 현재 질문으로 입력한 텍스트
+    const [answer, setAnswer] = useState(''); // @State, 현재 질문에 답변으로 나온 텍스트
+    const [loading, setLoading] = useState(false); // @State, 현재 로딩 상태
 
-    const searchParms = useSearchParams();
-    const imageurl = searchParms.get("image_url");
+    const searchParms = useSearchParams(); // 'home' 페이지에서 URL 쿼리
+    const imageurl = searchParms.get("image_url"); // S3에 업로드 된 이미지의 URL 파싱 
 
-    function onDocumentLoadSuccess({ numPages }) {
+    function onDocumentLoadSuccess({ numPages }) { // 페이지 불러오기 성공 시 현재 페이지로 설정 
         setNumPages(numPages);
     }
 
-    const handleMouseUp = () => {
-        const selection = window.getSelection();
-        const selectedText = selection.toString();
-        if (selectedText.length > 0) {
+    const handleMouseUp = () => { // 마우스의 Drag Action
+        const selection = window.getSelection(); // Drag한 영역 
+        const selectedText = selection.toString(); // 해당 영역 문자열로 변환
+        if (selectedText.length > 0) { // 변환 시 문자열이 비어있지 않다면 선택한 텍스트 설정 및 번역
             setSelectedText(selectedText);
             translateText(selectedText);
         }
     };
 
-    useEffect(() => {
+    useEffect(() => { //
         const textLayer = document.querySelector('.react-pdf__Page__textContent');
         if (textLayer) {
             textLayer.addEventListener('mouseup', handleMouseUp);
@@ -46,57 +45,60 @@ export default function UploadPage() {
         };
     }, [handleMouseUp]);
 
-    const translateText = async (text) => {
+    const translateText = async (text) => { // 번역 기능 
         try {
-            const response = await fetch('/api/translate', {
+            const response = await fetch('/api/translate', { // /api/translate 경로에 요청
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ text }),
             });
-            const data = await response.json();
-            setTranslatedText(data.translatedText);
-        } catch (error) {
+            const data = await response.json();  // json 형태로 요청에 대한 응답 받기
+            setTranslatedText(data.translatedText); // 해당 응답의 translatedText 부분을 쿼리해서 현재 번역된 텍스트로 설정 
+        } catch (error) { // 에러 처리 
             console.error('Error translating text:', error);
         }
     };
 
-    const summarizePaper = async (imageurl) => {
+    const summarizePaper = async (imageurl) => { // 요약 기능 
         try {
-            const response = await fetch("http://localhost:2000/summ", {
+            const response = await fetch("http://localhost:2000/summ", { // python 서버의 2000 포트의 summ 경로에 요청
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ url: imageurl }),
             });
-            const data = await response.json();
-            setSummarizedText(data.summarized);
-        } catch (error) {
+            const data = await response.json(); // json 형태로 요청에 대한 응답 받기
+            setSummarizedText(data.summarized); // 해당 응답의 summarized 부분을 쿼리해서 현재 요약된 텍스트로 설정 
+        } catch (error) { // 에러 처리
             console.error('Error summarizing text: ', error)
         }
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
-        setAnswer('');
-
-        // axios.post('/api/ollama', { question })
-        //     .then((response) => {
-        //         setAnswer(response.data.answer);
-        //     })
-        //     .catch((error) => {
-        //         console.error('Error fetching answer:', error);
-        //         setAnswer('Error fetching answer.');
-        //     })
-        //     .finally(() => {
-        //         setLoading(false);
-        //     });
-    };
+    const handleSubmit = async (e) => { // 사용자가 입력한 내용을 제출
+        e.preventDefault();  // 브라우저의 기본 동작을 막음
+        setLoading(true); // 로딩 활성화 
+        
+        try {
+            const reponse = await fetch('http://127.0.0.1:8000/llama/ask', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({question})
+            });
+            const data = await reponse.json();
+            setAnswer(data.answer)
+            //setAnswer('hello')
+        } catch(error) {
+            console.error('Error fetching the answer:', error)
+        }
+        
+     };
 
     return (
         <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
             <div style={{ width: '50%', overflowY: 'auto', borderRight: '1px solid #ccc', padding: '10px' }}>
                 <div style={{ textAlign: 'center', marginBottom: '10px' }}>
-                    <button onClick={() => setPageScale(pageScale >= 3 ? 3 : pageScale + 0.1)}>
+                    <button onClick={() => setPageScale(pageScale >= 3 ? 3 : pageScale + 0.1)}> 
                         Zoom In
                     </button>
                     <button onClick={() => setPageScale(pageScale <= 1 ? 1 : pageScale - 0.1)}>
@@ -113,6 +115,7 @@ export default function UploadPage() {
                 <div style={{ flex: 1, overflowY: 'auto', marginBottom: '10px' }}>
                     <h2>Q&A</h2>
                     {loading && <p>Loading...</p>}
+                    {translatedText && <p>Translated: {translatedText}</p>} {/* 번역된 텍스트 표시 */}
                     {answer && <p>Answer: {answer}</p>}
                 </div>
                 <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', padding: '10px', borderTop: '1px solid #ccc' }}>
