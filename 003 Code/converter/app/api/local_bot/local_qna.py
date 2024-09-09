bb054e7c468ed5a5bc6925817675bb423c6f0b32
yoonl22 <tylee0840@gmail.com>
2024-09-09 21:46:33 +0900

    빠름

--- 003 Code/converter/app/api/local_bot/local_qna.py
+++ 003 Code/converter/app/api/local_bot/local_qna.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException, APIRouter, Request
 import os
 import tempfile
 import requests
+import chromadb
+from uuid import uuid4
 from fastapi import FastAPI, HTTPException, Request, APIRouter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -10,33 +12,40 @@ from app.local_load.model_loader import load_llm_model, load_embedding_model  # 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+
+
 localQna = APIRouter(prefix='/localQna')
 
+
 # 모델을 한 번 로드하고 여러 요청에 재사용
-llm = load_llm_model()
+llm, tokenizer = load_llm_model()
 hfe = load_embedding_model()  # 임베딩 모델도 한 번 로드
+
 
 # 프롬프트 설정 (일반 질문, 요약, 번역)
 system_prompt_general = (
     "You are an assistant for general question-answering tasks. "
     "Provide accurate and concise information. Answer in Korean."
-    "\n\n"
-    "{context}"
+    "\n"
+    # "{context}"
 )
+
 
 system_prompt_summary = (
     "You are an assistant specialized in summarizing academic papers. "
     "Summarize the provided text in three to five sentences. Answer in Korean."
-    "\n\n"
-    "{context}"
+    "\n"
+    # "{context}"
 )
+
 
 system_prompt_translation = (
     "You are an assistant that translates English to Korean. "
     "Translate the following text accurately and naturally into Korean."
-    "\n\n"
-    "{context}"
+    "\n"
+    # "{context}"
 )
+
 
 # 질의 분석 함수: 사용자의 요청이 요약인지, 번역인지 결정
 def analyze_query(query):
@@ -47,6 +56,33 @@ def analyze_query(query):
         return "translation"
     else:
         return "general"
+
+
+def handle_retriever(path: str):
+    client = chromadb.PersistentClient("../../../db")
+    try:
+        vector_store = Chroma(client=client,
+                              collection_name=os.path.basename(path),
+                              embedding_function=hfe,
+                              create_collection_if_not_exists=False)
+    except:
+        loader = PyPDFLoader(path)
+        pages = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        chunks = text_splitter.split_documents(pages)
+
+        vector_store = Chroma(
+            client=client,
+            collection_name=os.path.basename(path),
+            embedding_function=hfe,
+            create_collection_if_not_exists=True
+        )
+        vector_store.add_documents(documents=chunks, ids=[str(uuid4()) for _ in range(len(chunks))])
+
+    retriever = vector_store.as_retriever()
+    return retriever
+
 
 # 프롬프트 선택 및 응답 생성 함수
 def handle_query(query, retriever):
@@ -62,14 +98,48 @@ def handle_query(query, retriever):
     
     # LLM 체인 생성
   
-    qa_chain = create_stuff_documents_chain(llm, chosen_prompt)
-    rag_chain = create_retrieval_chain(retriever, qa_chain)
+    # qa_chain = create_stuff_documents_chain(llm, chosen_prompt)
+    # rag_chain = create_retrieval_chain(retriever, qa_chain)
     
     # 질문에 대한 답변 생성
-    response = rag_chain.invoke({"input": query})
+    # response = rag_chain.invoke({"input": query})
+    contexts = retriever.invoke(query)
     
-    return response["answer"]
+    return contexts, chosen_prompt
 
+
+def generate_text(prompt, instruction, contexts):
+    tmp = ''
+    for context in contexts:
+        tmp += f"{context.page_content}\n"
+
+    SYSTEM_PROMPT = prompt
+    USER_PROMPT = f"""
+    Question: {instruction}\n\n
+    Context: {tmp}"""
+
+    messages = [
+        {"role": "system", "content": f"{SYSTEM_PROMPT}"},
+        {"role": "user", "content": f"{USER_PROMPT}"}
+    ]
+
+    prompt = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    generation_kwargs = {
+        "max_tokens":512,
+        "stop":["<|eot_id|>"],
+        "top_p":0.1,
+        "temperature":0.1,
+        "echo":True,
+    }
+
+    response_msg = llm(prompt, repeat_penalty=1.3, **generation_kwargs)
+    text = response_msg['choices'][0]['text'][len(prompt):]
+    return text
 
 
 # FastAPI 경로 처리
@@ -94,22 +164,25 @@ async def generate_answer(request: Request):
         pdf_file_path = tmp_file.name
 
     # PyPDFLoader를 사용하여 PDF 파일의 텍스트 추출
-    loader = PyPDFLoader(pdf_file_path)
-    documents = loader.load()
+    # loader = PyPDFLoader(pdf_file_path)
+    # documents = loader.load()
 
-    # 텍스트 분할
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(documents)
+    # # 텍스트 분할
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    # splits = text_splitter.split_documents(documents)
     
-    # 임베딩 및 벡터 저장소 설정
-    vectorstore = Chroma.from_documents(documents=splits, embedding=hfe)
-    retriever = vectorstore.as_retriever()
+    # # 임베딩 및 벡터 저장소 설정
+    # vectorstore = Chroma.from_documents(documents=splits, embedding=hfe)
+    # retriever = vectorstore.as_retriever()
+
+    retriever = handle_retriever(pdf_file_path)
 
     # 임시 파일 삭제
     os.remove(pdf_file_path)
 
     # 사용자의 질의를 처리하고 응답 생성
-    result = handle_query(text, retriever)
+    contexts, PROMPT = handle_query(text, retriever)
+    result = generate_text(PROMPT, text, contexts)
 
     # 응답 반환
     return {"answer": result}
--- 003 Code/converter/app/local_load/model_loader.py
+++ 003 Code/converter/app/local_load/model_loader.py
@@ -1,17 +1,20 @@
 # model_loader.py
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
+from transformers import AutoTokenizer
+from llama_cpp import Llama
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 # LLM 모델을 초기화하는 함수
 def load_llm_model():
-    llm = ChatOllama(
-        model="llama3.1:8b",
-        repeat_penalty=1.3,
-        seed=2024,
-        temperature=0.6,
-        top_p=0.5,
+    # model_id = 'MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M'
+    tokenizer = AutoTokenizer.from_pretrained('app/local_load/model')
+    llm = Llama(
+        model_path='app/local_load/model/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf', #다운로드받은 모델의 위치
+        n_ctx=4096,
+        n_gpu_layers=-1,        # Number of model layers to offload to GPU
+        verbose=False
     )
-    return llm
+    return llm, tokenizer
 
 # 임베딩 모델을 초기화하는 함수
 def load_embedding_model():
