af9b40f09bdedd3df9653c21c1ed316d1a1327b5
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-10-23 21:55:44 +0900

    페이지는 정상 작동, 파일 임베딩 x

--- 003 Code/converter/app/local_load/model_loader.py
+++ 003 Code/converter/app/local_load/model_loader.py
@@ -4,23 +4,23 @@ import chromadb
 from chromadb.utils import embedding_functions
 from llama_cpp import Llama
 from transformers import AutoTokenizer
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-MODEL_PATH = 'app/mlp_llama_model/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf'
+MODEL_PATH = 'app/local_load/model/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf'
 EMBED_MODEL_ID = 'BAAI/bge-m3'
 CHROMA_CLIENT_PATH = "../../../db"
 
 class RAG():
 
-    model = None
+    # model = None
     tokenizer = None
     embeddings = None
     client = None
     collection = None
 
-    def __init__(self):
+    def __init__(self, file_name: str):
         # Init model
         self.tokenizer = AutoTokenizer.from_pretrained('app/local_load/model')
         self.model = Llama(
@@ -36,14 +36,14 @@ class RAG():
 
         self.client = chromadb.PersistentClient(path=CHROMA_CLIENT_PATH)
 
-
-    def add_chunks(self, path: str) -> None:
         self.collection = self.client.create_collection(
-                name=path.split('.')[0],
+                name=file_name,
                 embedding_function=self.embeddings,
                 metadata={'hnsw:space': 'cosine'}
             )
 
+
+    def add_chunks(self, path: str) -> None:
         loader = PyPDFLoader(path)
         pages = loader.load()
 
@@ -114,23 +114,23 @@ class RAG():
         return text
 
 
-# # LLM 모델을 초기화하는 함수
-# def load_llm_model():
-#     # model_id = 'MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M'
-#     tokenizer = AutoTokenizer.from_pretrained('app/local_load/model')
-#     llm = Llama(
-#         model_path='app/local_load/model/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf', #다운로드받은 모델의 위치
-#         n_ctx=4096,
-#         n_gpu_layers=-1,        # Number of model layers to offload to GPU
-#         verbose=False
-#     )
-#     return llm, tokenizer
+# LLM 모델을 초기화하는 함수
+def load_llm_model():
+    # model_id = 'MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M'
+    tokenizer = AutoTokenizer.from_pretrained('app/local_load/model')
+    llm = Llama(
+        model_path='app/local_load/model/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf', #다운로드받은 모델의 위치
+        n_ctx=4096,
+        n_gpu_layers=-1,        # Number of model layers to offload to GPU
+        verbose=False
+    )
+    return llm, tokenizer
 
-# # 임베딩 모델을 초기화하는 함수
-# def load_embedding_model():
-#     hfe = HuggingFaceBgeEmbeddings(
-#         model_name="BAAI/bge-m3",
-#         model_kwargs={"device": "cpu"},
-#         encode_kwargs={"normalize_embeddings": True}
-#     )
-#     return hfe
+# 임베딩 모델을 초기화하는 함수
+def load_embedding_model():
+    hfe = HuggingFaceBgeEmbeddings(
+        model_name="BAAI/bge-m3",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
+    )
+    return hfe
